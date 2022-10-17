6a719bcecebc72b7304ace4eab484baf5f8f9556
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-10-17 11:33:13 +0300

    feat: protected route Hotels

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+import unpackLocalStorage from '@/utils/unpackLocalStorage';
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -9,7 +10,10 @@ const routes = [
   {
     path: '/hotels',
     name: 'hotels',
-    component: () => import('../views/HotelsView.vue')
+    component: () => import('../views/HotelsView.vue'),
+    meta: {
+      isLogined: true,
+    }
   },
   {
     path: '/:pathMatch(.*)*',
@@ -22,5 +26,20 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+// защита роута
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.isLogined)) {
+    const [isExist, { isLogined }] = unpackLocalStorage('hotels-app');
+    if (isExist && isLogined) {
+      next(true);
+    } else {
+      // если user is not logined, то редирект на авторизацию
+      next({ name: 'login' });
+    }
+  } else {
+    next(true);
+  }
+});
 
 export default router
